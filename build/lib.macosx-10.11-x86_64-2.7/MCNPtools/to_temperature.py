9e1e1ee98997d8863c427c1bec5b6db6e45e559b
Ryan M. Bergmann <ryanmbergmann@gmail.com>
2018-08-29 17:16:57 +0200

    removed build directory, oops

--- build/lib.macosx-10.11-x86_64-2.7/MCNPtools/to_temperature.py
+++
@@ -1,4 +0,0 @@
-def to_temperature(E_in):
-	### assumes MeV, gives kelvin
-	import numpy
-	return numpy.multiply(11604.50520 , 1.0e6*E_in)
--- build/lib.macosx-10.11-x86_64-2.7/MCNPtools/to_wavelength.py
+++
@@ -1,4 +0,0 @@
-def to_wavelength(E_in):
-	### assumes MeV, gives Angstrom
-	import numpy
-	return numpy.divide(2.86014369e-4, numpy.sqrt(numpy.array(E_in)))
