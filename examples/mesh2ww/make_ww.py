435536358132dc0522e7930d819efa8e35ed5275
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2018-09-06 14:12:45 +0200

    adding mesh2ww example

---
+++ examples/mesh2ww/make_ww.py
@@ -0,0 +1,13 @@
+#! /home/l_bergmann/anaconda/bin/python -W ignore
+
+
+
+from MCNPtools.mctal import mctal
+tal=mctal('./case001-noww.mctal')
+tal.write_weight_windows_from_meshtal(output='2dflux.wwinp',
+                                      tals =[[101]     ,[111]    ,'e','/','*','z'],
+                                      norms=[[0.5*5e-3],[0.5*1.0],'' ,'' ,'' ,'' ],
+                                      energies=  [[],[],[],[],[],[]],
+                                      normpoints=[[],[],[],[],[],[]],
+                                      mask_weight=[[  0.],[ 0.],[ 0.],[ 0.],[ 0.],[ 0.]],
+                                      kill_weight=[[ 10.],[10.],[10.],[10.],[10.],[10.]])
